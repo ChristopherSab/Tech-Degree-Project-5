23faa954caf64f25be4be0201227abaca45ef517
ChristopherSab <chris.sabaoth@protonmail.com>
2020-03-14 19:13:51 +0000

    Added Jquery filter search - NOT WORKING

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-lightbox.option({
+
+
+$(document).ready(function(){
+  $("#search-input").keyup(function(){
+
+      // Retrieve the input field text 
+      var filter = $(this).val();
+
+      // Loop through the comment list
+      $("[data-alt]").each()(function(){
+
+          // If the list item does not contain the text phrase fade it out
+          if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+              $(this).fadeOut();
+
+          // Show the list item if the phrase matches and increase the count by 1
+          } else {
+              $(this).show();
+          }
+      });
+
+  });
+});
+
+
+  lightbox.option({
     'resizeDuration': 200,
     'wrapAround': true,
   })
